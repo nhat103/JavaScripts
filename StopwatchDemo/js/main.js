808c3de9100556a35cc73d716ee846c31b6d6313
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-06-24 10:03:10 +0700

    Init commit

---
+++ StopwatchDemo/js/main.js
@@ -0,0 +1,13 @@
+//pseudocode for stopwatch
+add id stop , start
+display
+start 
+hour =00;
+min = 00;
+second = 00;
+
+  -> second ++ if second = 60 min ++,  second =0
+  if min = 60-> hour ++, min = 0
+    if second <=9 second = 0+ second
+  ===============================================
+
